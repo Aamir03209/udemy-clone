abfd37d16526cf44f5639947a259708a56e01faa
Amir03209 <ah03209120917>
2024-04-01 00:50:37 -0700

    completed signup and signin pages

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react"
+
+const AuthLayout=({
+    children
+}:{
+    children:React.ReactNode
+})=>{
+return(
+    <div className="h-full flex justify-center items-center">
+        {children}
+    </div>
+)
+}
+export default AuthLayout
